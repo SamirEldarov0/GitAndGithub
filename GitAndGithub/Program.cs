3b908e50600d5601c1e886ebf5f168310684f791
SamirEldarov0 <eldarovsamir535@gmail.om>
2024-12-24 00:55:50 +0400

    .gitignore,gitandgithub.slnetc

---
+++ GitAndGithub/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GitAndGithub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Salam Baku
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
